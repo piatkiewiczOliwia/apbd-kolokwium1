1ae61e616e06369d1f96c1dc005c7e64079afb03
Oliwia Piątkiewicz <piatkiewicz.oliwia0606@gmail.com>
2024-05-07 13:48:45 +0200

    k3

---
+++ kolokwium1/kolokwium1/Program.cs
@@ -0,0 +1,29 @@
+using kolokwium1.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//DODAJ REPO
+builder.Services.AddControllers();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
